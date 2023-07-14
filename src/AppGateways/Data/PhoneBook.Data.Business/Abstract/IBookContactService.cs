c664efc5e6528d147c5e16c98852cde94976c81e
Veli Yavuz Göçeren <vygoceren@gmail.com>
2023-07-14 21:58:12 +0300

    Data Api Methods Created

--- src/AppGateways/Data/PhoneBook.Data.Business/Abstract/IBookContactService.cs
+++ src/AppGateways/Data/PhoneBook.Data.Business/Abstract/IBookContactService.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using PhoneBook.Data.Core.ResponseTypes;
+using PhoneBook.Data.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,10 @@ namespace PhoneBook.Data.Business.Abstract
 {
     public interface IBookContactService
     {
+        Task<Response<BookContact>> GetBookContactById(Guid uuid);
+        Task<Response<BookContact>> Insert(BookContact entity);
+        Task<Response<BookContact>> Update(BookContact entity);
+        Task<Response<BookContact>> Delete(Guid uuid);
+        Task<Response<List<BookContact>>> GetList(Expression<Func<BookContact, bool>> filter = null);
     }
 }
